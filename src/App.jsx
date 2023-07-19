46dd8d01fee9913a03ef2a8ee806aa26249e0b4f
Michael Nascimento <michael.nsc@outlook.com>
2023-07-19 20:40:43 -0300

    adiciona estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import Botao from "./components/Botao";
+import Input from "./components/Input";
 
 const App = () => {
-  return <Botao titulo="Título do botão" cor="green" />;
+  return (
+    <>
+      <Botao titulo="Título do botão" cor="green" />
+      <Input
+        label="Teste"
+        id="123"
+        onChange={(e) => console.log(e.target.value)}
+      />
+    </>
+  );
 };
 
 export default App;
