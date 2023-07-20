62b714806f1738187e8454a596bf3300d590eb9c
Michael Nascimento <michael.nsc@outlook.com>
2023-07-19 22:05:47 -0300

    implementa navegacao de cadastro - login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Cadastro from "./paginas/Cadastro";
+import ListaProdutos from "./paginas/ListaProdutos";
 import Login from "./paginas/Login";
 
 const App = () => {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/lista-produtos" element={<ListaProdutos />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
