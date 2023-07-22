624f5cf2df8cb7f07261d766f202b5690585b07f
Michael Nascimento <michael.nsc@outlook.com>
2023-07-22 15:57:38 -0300

    adicionar atualização de produto

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,27 @@ const App = () => {
         <Route path="produtos" element={<MenuOutlet />}>
           <Route index={true} element={<ListaProdutos />} />
           <Route path="novo" element={<CadastrarProduto />} />
+          {/* 
+            A rota abaixo é a rota de edição de produtos. Ela recebe um parâmetro
+            chamado "id" na URL. Para acessar o valor deste parâmetro, usamos o
+            hook "useParams" do React Router DOM.
+
+            Exemplo: /produtos/editar/1
+            O valor de "id" será 1. 
+            (ver arquivo CadastrarProdutos/index.jsx para entender a aplicação)
+
+            Importante: a prop modoEdicao é passada para o componente CadastrarProduto
+            para que ele saiba se está no modo edição ou não.
+
+            Você pode, opcionalmente, adicionar somente o nome da prop, sem o valor, já
+            que, ao existir a prop, o valor inferido pelo React é "true".
+
+            Exemplo: <Route path="editar/:id" element={<CadastrarProduto modoEdicao />} />
+          */}
+          <Route
+            path="editar/:id"
+            element={<CadastrarProduto modoEdicao={true} />}
+          />
         </Route>
         <Route path="*" element={<PaginaNaoEncontrada />} />
       </Routes>
--- src/paginas/CadastrarProduto/index.jsx
+++ src/paginas/CadastrarProduto/index.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Conteiner from "../../componentes/Conteiner";
 import Input from "../../componentes/Input";
 
 import Botao from "../../componentes/Botao";
 import "./cadastrarProduto.css";
 
-const CadastrarProduto = () => {
+// Esta página pode ser utilizada tanto para cadastro de produtos
+// quanto para edição de produtos. Basta informar a propriedade "modoEdicao: true"
+// para o componente <CadastrarProduto /> executar o useEffect, buscar o produto
+// pelo ID e preencher o formulário com os dados do produto.
+//
+// Aqui, inicializamos a prop com o valor \/ "false"
+const CadastrarProduto = ({ modoEdicao = false }) => {
   const navegar = useNavigate();
 
   const [form, setForm] = useState({
@@ -47,11 +53,59 @@ const CadastrarProduto = () => {
     });
   };
 
+  const enviarAtualizacaoProduto = () => {
+    fetch(`http://localhost:3000/carros/${produtoId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form), // o form já possui a estrutura que o back end necessita
+    }).then((response) => {
+      if (response.ok) {
+        alert("Produto atualizado com sucesso!");
+        navegar("/produtos"); // navega de volta para a lista de produtos
+      } else {
+        alert("Erro ao atualizar o produto!");
+      }
+    });
+  };
+
+  // este hook do React Router retorna um objeto com os parâmetros da URL
+  // Exemplo: /editar/1
+  // 1 é o ID do produto que queremos editar
+  const produtoId = useParams().id;
+  // se não existir o parâmetro "id" na URL, o valor de produtoId será undefined
+
+  // caso esteja no modo criação (url: /produtos/novo), não precisamos buscar o produto
+  // pelo ID, então o useEffect não será executado.
+  useEffect(() => {
+    // se a prop "modoEdicao" for true, então estamos editando um produto
+    if (modoEdicao) {
+      // buscamos o produto pelo ID
+      fetch(`http://localhost:3000/carros/${produtoId}`)
+        .then((response) => response.json())
+        .then((produto) => {
+          // atualizamos o estado "form" com os dados do produto
+          // Isto só funciona porque o nome das chaves do objeto "form"
+          // são iguais aos nomes das chaves do objeto "produto" que vem
+          // do back end.
+          setForm(produto);
+        });
+    }
+    // precisamos informar a dependência "modoEdicao" para que o useEffect
+    // seja executado toda vez que o valor de "modoEdicao" for alterado.
+  }, [modoEdicao, produtoId]);
+
   return (
     <>
       <Conteiner>
         <Link to={-1}>Voltar</Link>
-        <h1 className="CadastrarProduto__titulo">Cadastrar Produto</h1>
+        <h1 className="CadastrarProduto__titulo">
+          {/* 
+            Se a prop "modoEdicao" for true, o título será "Editar Produto",
+          */}
+          {modoEdicao ? "Cadastrar" : "Editar"} Produto
+        </h1>
         <div className="CadastrarProduto__conteudo">
           <Input
             id="modelo"
@@ -83,7 +137,22 @@ const CadastrarProduto = () => {
             valor={form.foto}
             aoMudar={atualizaForm}
           />
-          <Botao aoClicar={enviarCadastroProduto} titulo="Cadastrar Produto" />
+          {/* 
+            Se a prop "modoEdicao" for true, o botão será "Atualizar Produto",
+            caso contrário, será "Cadastrar Produto". A função que será executada
+            ao clicar no botão também muda de acordo com o valor da prop "modoEdicao".
+          */}
+          {modoEdicao ? (
+            <Botao
+              aoClicar={enviarAtualizacaoProduto}
+              titulo="Atualizar Produto"
+            />
+          ) : (
+            <Botao
+              aoClicar={enviarCadastroProduto}
+              titulo="Cadastrar Produto"
+            />
+          )}
         </div>
       </Conteiner>
     </>
